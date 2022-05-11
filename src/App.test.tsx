e8e1981d6e90de3804bdaefb841903ea088d218e
Nate S <nshiff@users.noreply.github.com>
2022-05-10 22:03:22 -0400

    removing items not used for demo

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import CounterDemo from "./App";
-
-test("renders learn react link", () => {
-  render(<CounterDemo />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
